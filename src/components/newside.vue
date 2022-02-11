558db2e469e69d1c3fea1bfb36d9c4a36c9e95f1
Jay <jay05thakare@gmail.com>
2022-02-12 00:44:12 +0530

    Campaign page, popup, orders page, related comp

---
+++ src/components/newside.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="pl-52">
+ubnjsvfkd
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Newside',
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/AllOrders.vue
+++ src/views/AllOrders.vue
@@ -3,8 +3,10 @@
         <main class="flex-1">
         <div class="py-6 space-y-4">
             <div class=" mx-auto px-4 sm:px-6 md:px-8 flex justify-between">
-                <h1 class="text-lg font-semibold text-dark-text">Orders</h1>
-                <button class=" text-sm font-medium text-dark-text">Export</button>
+                <h1 class="pt-8 xs:pt-0 text-lg font-semibold text-dark-text">Orders</h1>
+                <div class="xs:static absolute top-0 right-0">
+                  <CreateCampaignButton />
+                </div>
             </div>
           <div class=" mx-auto px-4 sm:px-6 md:px-8 space-y-8 flex flex-col w-full">
             <!-- Replace with your content -->
